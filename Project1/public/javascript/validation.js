59d61c22523f1406cea99129ce78bccfe59a4166
diwashamatya <51187292+diwashamatya@users.noreply.github.com>
2023-02-23 21:25:53 +0545

    Add files via upload

---
+++ Project1/public/javascript/validation.js
@@ -0,0 +1,17 @@
+
+
+
+function validation() {
+    var username = document.getElementById('username').value;
+
+    if (username == "") {
+        alert("Enter the email.");
+    }
+
+    var password = document.getElementById('password').value;
+    if (password == "") {
+        alert("Enter the password.")
+    }
+
+
+}
